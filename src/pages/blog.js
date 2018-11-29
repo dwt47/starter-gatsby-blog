a8cabd8393c7a2766f63dc35045a6884d7ce55be
Daniel Thompson <dwt02011@mymail.pomona.edu>
2018-11-28 21:03:05 -0500

    Dummy change #2

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -14,7 +14,7 @@ class BlogIndex extends React.Component {
       <div style={{ background: '#fff' }}>
         <Helmet title={siteTitle} />
         <div className={styles.hero}>
-          Blog Hi there
+          Blog Hi there dummy change
         </div>
         <div className="wrapper">
           <h2 className="section-headline">Recent articles</h2>
