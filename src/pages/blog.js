c657b4c309d5a4fc953cad25d6af76c2ba7f2f62
Daniel Thompson <dwt02011@mymail.pomona.edu>
2018-11-28 22:50:21 -0500

    Added bluehost config to work with CI?

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -14,7 +14,7 @@ class BlogIndex extends React.Component {
       <div style={{ background: '#fff' }}>
         <Helmet title={siteTitle} />
         <div className={styles.hero}>
-          Omg is this just gonna...work
+          Changed.
         </div>
         <div className="wrapper">
           <h2 className="section-headline">Recent articles</h2>
